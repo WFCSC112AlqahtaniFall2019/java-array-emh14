e1341e2bdfb36f2c9dd0f6961f5b0c8c1641b428
ethanharrison <harrem18@wfu.edu>
2019-12-03 15:11:23 -0500

    Get input from the user

--- ArrayCylinders.java
+++ ArrayCylinders.java
@@ -5,7 +5,15 @@ public class ArrayCylinders {
     public static void main ( String [] args ){
        Scanner in = new Scanner(System.in);
        Cylinder list []= new Cylinder[5] ;
+
        //1) fill the information for each Cylinder from the user
+
+        for (int i = 1; i < 6; i++) {
+            double radius = in.nextDouble();
+            double height = in.nextDouble();
+
+            list[i] = new Cylinder(radius, height);
+        }
 
 
         // 2)print the information of all Cylinders
