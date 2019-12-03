3e9f5a13fa3d5de9eb9ed654e8f3f1b50fe83d8f
ethanharrison <harrem18@wfu.edu>
2019-12-03 15:15:47 -0500

    Print info

--- ArrayCylinders.java
+++ ArrayCylinders.java
@@ -8,15 +8,22 @@ public class ArrayCylinders {
 
        //1) fill the information for each Cylinder from the user
 
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("Enter radius for Cylinder " + i);
             double radius = in.nextDouble();
+            System.out.println("Enter height for Cylinder " + i);
             double height = in.nextDouble();
 
             list[i] = new Cylinder(radius, height);
         }
 
-
         // 2)print the information of all Cylinders
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Volume for Cylinder " + i + ":");
+            list[i].print();
+        }
 
 
     }
